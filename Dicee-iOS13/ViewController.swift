4c6dd4ad81c03532a2f2ab790655eba0d94cfd95
olugbengalium <olugbengalium@gmail.com>
2022-10-16 17:20:07 +0100

    The completed Dice Rolling Project

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -7,14 +7,23 @@
 //
 
 import UIKit
+//class UIImage : NSObject {
+//
+//}
+// THIS WILL WORK FOR XCODE 14 INSTEAD OF THE IMAGE LITERAL
 
 class ViewController: UIViewController {
+    
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var diceImage1: UIImageView!
+    @IBOutlet weak var diceImage2: UIImageView!
+    @IBAction func rollButton(_ sender: UIButton) {
+//        var i = Int.random(in: 0...5)
+//        var j = Int.random(in: 0...5)
+        let diceArray = ["DiceOne", "DiceTwo", "DiceThree", "DiceFour", "DiceFive", "DiceSix"]
+        diceImage2.image = UIImage(named: diceArray.randomElement()!) //OPTIONAL UNWRAPPING
+        diceImage1.image = UIImage(named: diceArray.randomElement()!) //OPTIONAL UNWRAPPING
+
     }
-
-
+    
 }
-
